da39890e3477efc6222de97f9c3ba6fe2687b482
Theodor Vararu <theo@vararu.org>
2015-07-10 01:15:51 +0100

    Move to devDependencies, fix build.

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
+/* global fetch */
 import React from 'react'
 import Link from './Link'
-const fetch = window.fetch
 
 export default class Home extends React.Component {
   static displayName = 'Home'
