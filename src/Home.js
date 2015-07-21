192ae39924642dee6d3ccb5390557c498dae72ee
Theodor Vararu <theo@vararu.org>
2015-07-21 21:52:51 +0100

    Remove fetch experiment.

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,8 @@
-/* global fetch */
 import React from 'react'
 import Link from './Link'
 
 export default class Home extends React.Component {
   static displayName = 'Home'
-  constructor (props) {
-    super(props)
-    this.fetchData()
-  }
-  async fetchData () {
-    const result = await fetch('https://api.github.com/repos/tvararu/cv')
-    console.log(result)
-  }
   render () {
     return (<div>
       <Link href='https://github.com/tvararu'>GitHub</Link>
